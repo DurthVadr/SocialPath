ede0e0eb447f0f90d9d9e9fa09b4f61b10998d48
Mertcan Sağlam <92371591+DurthVadr@users.noreply.github.com>
2023-04-09 17:15:53 +0300

    Add files via upload

---
+++ lib/utilities/list_of_answers.dart
@@ -0,0 +1,62 @@
+const List<String> optionOne = [
+  '',
+  '1a', // =>Correct Answer For Question NO:1
+
+  '2a',
+
+  '3a',
+
+  '4a',
+
+  '5a', // =>Correct Answer For Question NO:5
+
+  '6a',
+
+  '7a', // =>Correct Answer For Question NO:7
+
+  '8a',
+
+  '9a',
+
+  '10a'
+];
+const List<String> optionTwo = [
+  '',
+  '1b',
+  '2b',
+  '3b', // =>Correct Answer For Question NO:3
+  '4b', // =>Correct Answer For Question NO:4
+  '5b',
+  '6b',
+  '7b',
+  '8b',
+  '9b',
+  '10b',
+  'Formulation',
+];
+List<String> optionThree = [
+  '',
+  'VR Box',
+  'Natural intelligence', // =>Correct Answer For Question NO:2
+  'Data',
+  '1912',
+  'AI Effect',
+  'Input',
+  'Mastering',
+  'Mathematics', // =>Correct Answer For Question NO:8
+  'Knowledge engineering', // =>Correct Answer For Question NO:9
+  'Ability',
+];
+List<String> optionFour = [
+  '',
+  'Amazon Alexa',
+  'Cognition',
+  'Processor',
+  '1909',
+  'Machination',
+  'Neural networks', // =>Correct Answer For Question NO:6
+  'Data',
+  'French',
+  'Mastering',
+  'Machine learning', // =>Correct Answer For Question NO:10
+];
